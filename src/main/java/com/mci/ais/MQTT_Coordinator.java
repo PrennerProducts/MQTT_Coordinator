5077635ebd6d5c5f2217db365e3545aa2c436642
Lukas Prenner <prenner.products@gmail.com>
2023-03-17 20:30:34 +0100

    MQTT Coordinator finished

--- src/main/java/com/mci/ais/MQTT_Coordinator.java
+++ src/main/java/com/mci/ais/MQTT_Coordinator.java
@@ -42,7 +42,7 @@ public class MQTT_Coordinator implements MqttCallback {
     public void messageArrived(String topic, MqttMessage message) throws Exception {
         String[] topicparts = topic.split("/");
         if (topicparts[0].equals("coordinator")) {
-            if (topicparts.length > 2) {
+            if (topicparts.length ==3 )  {
                 // New Request for Darts to work with
                 if (topicparts[1].equals("request")) {
                     System.out.println("New request for Darts");
@@ -50,14 +50,17 @@ public class MQTT_Coordinator implements MqttCallback {
                     if (totalDarts > requestedDarts) {
                         long darts = Math.min(totalDarts - requestedDarts, dartsProRequest);
                         msg = "OK:" + darts;
-                        System.out.println("OK: Worker recives: " + darts + " darts");
-                    } else {
+                        System.out.println("OK: Worker " + topicparts[2] +" recives: " + darts + " darts");
+                        requestedDarts += darts;
+                        System.out.println("******************** Topicspart[2=)" + topicparts[2]);
+                    }else {
                         msg = "NOK";
                         System.out.println("NOK: No more darts available");
+                        Thread.sleep(1000);
                     }
                     MqttMessage sendMessage = new MqttMessage(msg.getBytes());
-                    mqtt.publish("worker/" + topicparts[2], message);
-                    System.out.println("DEBUG: message: " + message + " sent to " + topicparts[2]);
+                    mqtt.publish("worker/" + topicparts[2], sendMessage);
+                    System.out.println("DEBUG: message: " + msg + " sent to " + topicparts[1]);
                 }
                 // New message with results from a Worker for our calculation
             } else if (topicparts[1].equals("result")) {
@@ -70,6 +73,9 @@ public class MQTT_Coordinator implements MqttCallback {
                     System.out.println("Worker " + topicparts[1] + Arrays.toString(msgparts));
                     resultTotal += total;
                     hitsTotal += hits;
+                }
+                else if (msgparts.length == 1) {
+                    System.out.println("***********************HELLO ME FRIEND");
                 }else {
                     System.err.println("Unknown message Type!");
                 }
@@ -80,7 +86,7 @@ public class MQTT_Coordinator implements MqttCallback {
 
     @Override
     public void deliveryComplete(IMqttDeliveryToken token) {
-        System.out.println("DeliveryComplete" + token);
+        System.out.println("DeliveryComplete");
 
     }
 
@@ -88,9 +94,17 @@ public class MQTT_Coordinator implements MqttCallback {
     public void work() throws InterruptedException {
         System.out.println("Cordinator listens @localhost:1883 for MQTT messages!");
         while (totalDarts > resultTotal) {
-            Thread.sleep(200);
+            Thread.sleep(1000);
+
         }
         // Calculate Pi
+        double pi = 4.0 * hitsTotal/totalDarts;
+        System.out.println("###########################################################################");
+        System.out.println("###########################____RESULT____##################################");
+        System.out.println("###########################################################################");
+        System.out.println("PI= " + pi);
+        System.out.println("###########################################################################");
+
     }
 
     public static void main(String[] args) throws MqttException {
